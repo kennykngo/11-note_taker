194ef5d8710ae9abe0e1e570dd8c0bc35b125e7f
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-08-27 13:36:51 -0700

    first commit

---
+++ routes/api-routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+router.get("/api/notes", (req, res) => {
+  let data = fs.readFileSync("../db/db.json", "utf-8");
+  data = JSON.parse(data);
+  res.json(data);
+});
+
+router.post("/api/notes", (req, res) => {
+  let data = fs.readFileSync("../db/db.json", "utf8");
+  notes = JSON.parse(data);
+  let id = notes.length + 1;
+  const { title, text } = req.body;
+  notes.push({ title, text, id });
+  fs.writeFileSync("../db/db.json", JSON.stringify(notes, null, 2));
+});
+
+router.delete("/api/notes/:id", async (req, res) => {
+  let data = await fs.readFileSync("../db/db.json", "utf8");
+  let notes = JSON.parse(data);
+  const deleteId = req.params.id;
+  notes.forEach((value, index) => {
+    if (value.id == deleteId) {
+      notes.splice(index, 1);
+    }
+  });
+  await fs.writeFileSync(
+    "../db/db.json",
+    JSON.stringify(notes, null, 2),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+});
+
+module.exports = router;

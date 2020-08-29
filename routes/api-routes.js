dbc9d26e74fc26147b4c209c27a5dda2cbc42bbd
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-08-29 10:30:20 -0700

    fixed the save button, removed store.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,22 +3,23 @@ const router = express.Router();
 const fs = require("fs");
 
 router.get("/api/notes", (req, res) => {
-  let data = fs.readFileSync("../db/db.json", "utf-8");
+  let data = fs.readFileSync("./db/db.json", "utf-8");
   data = JSON.parse(data);
   res.json(data);
+  console.log(data);
 });
 
 router.post("/api/notes", (req, res) => {
-  let data = fs.readFileSync("../db/db.json", "utf8");
+  let data = fs.readFileSync("./db/db.json", "utf8");
   notes = JSON.parse(data);
   let id = notes.length + 1;
   const { title, text } = req.body;
   notes.push({ title, text, id });
-  fs.writeFileSync("../db/db.json", JSON.stringify(notes, null, 2));
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2));
 });
 
 router.delete("/api/notes/:id", async (req, res) => {
-  let data = await fs.readFileSync("../db/db.json", "utf8");
+  let data = await fs.readFileSync("./db/db.json", "utf8");
   let notes = JSON.parse(data);
   const deleteId = req.params.id;
   notes.forEach((value, index) => {
@@ -27,7 +28,7 @@ router.delete("/api/notes/:id", async (req, res) => {
     }
   });
   await fs.writeFileSync(
-    "../db/db.json",
+    "./db/db.json",
     JSON.stringify(notes, null, 2),
     (err) => {
       if (err) throw err;
